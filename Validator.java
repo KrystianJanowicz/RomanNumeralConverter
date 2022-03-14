abd5ad2c2c0d676f32653a5e085a6478f47da16f
KrystianJanowicz <krystianjanowicz97@gmail.com>
2022-03-14 23:32:02 +0100

    added calculating sequences with substraction

--- Validator.java
+++ Validator.java
@@ -1,17 +1,19 @@
+import java.util.Arrays;
+
 public class Validator {
 
     static char[] romanSigns = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
     static String rom = "MMMIDI";
-    static int[] romanSignValues = {1, 5, 10, 50, 100, 500, 1000};
-
-    static String rom2 = "MMCAXVIXIRI";
 
     public static void main(String[] args) {
 
-//        System.out.println(calcRomanToDecimal(rom));
-//        System.out.println("is incorrect?: " + containsIncorrectSigns(rom2));
+        char[] tablica = rom.toCharArray();
+        int i = markSubtraction(rom);
+        tablica[i + 1] = 'O';
+        tablica[i] = 'O';
 
-        System.out.println(containsSubtraction(rom));
+        System.out.println(calcAdditivePartToDecimal(Arrays.toString(tablica)));
+        System.out.println("roznica: " + calcSubtractionPartToDecimal(rom));
     }
 
     public static boolean validateRoman(String roman) {
@@ -19,15 +21,20 @@ public class Validator {
         return true;
     }
 
+    public static int calcSubtractionPartToDecimal(String sequence) {
+        char array[] = sequence.toCharArray();
+        return calcAdditivePartToDecimal(array[markSubtraction(sequence) + 1]) - calcAdditivePartToDecimal(array[markSubtraction(sequence)]);
+    }
+
     public static boolean containsIncorrectSigns(String sequence) {
         for (char sign : sequence.toCharArray()) {
-            if ((sign != 'M') || (sign != 'D') || (sign != 'C') || (sign != 'X') || (sign != 'V') || (sign != 'I'))
+            if ((sign != 'M') && (sign != 'D') && (sign != 'C') && (sign != 'X') && (sign != 'V') && (sign != 'I'))
                 return true;
         }
         return false;
     }
 
-    public static boolean containsSubtraction(String sequence) {
+    public static int markSubtraction(String sequence) {
         char[] charSequence = sequence.toCharArray();
         for (int i = 0; i < charSequence.length; i++) {
             for (int j = 0; i <= romanSigns.length; j++) {
@@ -39,20 +46,45 @@ public class Validator {
                                     (charSequence[i + 1] == romanSigns[j + 4]) ||
                                     (charSequence[i + 1] == romanSigns[j + 5]) ||
                                     (charSequence[i + 1] == romanSigns[j + 6]))) {
-                        System.out.println("znak " + charSequence[i] + " jest przed znakiem " + charSequence[i+1]);\
-
-                        return true;
+                        return i;
                     }
                 } catch (ArrayIndexOutOfBoundsException n) {
                     break;
                 }
             }
         }
-        return false;
+        return -1;
     }
 
+    public static int calcAdditivePartToDecimal(char rom) {
+        int decimal = 0;
+        switch (rom) {
+            case 'M':
+                decimal = decimal + 1000;
+                break;
+            case 'D':
+                decimal = decimal + 500;
+                break;
+            case 'C':
+                decimal = decimal + 100;
+                break;
+            case 'L':
+                decimal = decimal + 50;
+                break;
+            case 'X':
+                decimal = decimal + 10;
+                break;
+            case 'V':
+                decimal = decimal + 5;
+                break;
+            case 'I':
+                decimal = decimal + 1;
+                break;
+        }
+        return decimal;
+    }
 
-    public static int calcRomanToDecimal(String rom) {
+    public static int calcAdditivePartToDecimal(String rom) {
         int decimal = 0;
         for (char romanSign : romanSigns) {
             switch (romanSign) {
@@ -64,6 +96,9 @@ public class Validator {
                     break;
                 case 'C':
                     decimal = decimal + 100 * countSignsInString(rom, romanSign);
+                    break;
+                case 'L':
+                    decimal = decimal + 50 * countSignsInString(rom, romanSign);
                     break;
                 case 'X':
                     decimal = decimal + 10 * countSignsInString(rom, romanSign);
@@ -81,7 +116,6 @@ public class Validator {
 
     public static int countSignsInString(String line, char sign) {
         int outcome = line.length() - line.replace(String.valueOf(sign), "").length();
-        System.out.println("Number of letter " + sign + " in that sequence is " + outcome);
         return outcome;
     }
 }
